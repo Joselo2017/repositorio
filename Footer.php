c0a1c68421153316aa08ea83eb8ccca36be84cb4
Jose Luis Ospino Cueva <jlospino@serdan.com.co>
2017-06-23 10:26:12 -0500

    footer archivo

---
+++ Footer.php
@@ -0,0 +1,70 @@
+<footer class="page_footer ds ms section_padding_top_130 section_padding_bottom_100">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-6 to_animate text-center" data-animation="fadeInUp">
+							<div class="widget_text widget">
+								<a href="./" class="logo logo_image bottommargin_20">
+									<img src="images/logo-dark.png" alt="">
+								</a>
+								<p>
+									una alternativa real en el diseño de soluciones integrales.
+								</p>
+								<a href="#" class="social-icon theme-color-icon border-icon rounded-icon soc-twitter">&nbsp;</a>&nbsp;
+								<a href="#" class="social-icon theme-color-icon border-icon rounded-icon soc-facebook">&nbsp;</a>&nbsp;
+								<a href="#" class="social-icon theme-color-icon border-icon rounded-icon soc-google">&nbsp;</a>&nbsp;
+								<a href="#" class="social-icon theme-color-icon border-icon rounded-icon soc-linkedin">&nbsp;</a>&nbsp;
+								<a href="#" class="social-icon theme-color-icon border-icon rounded-icon soc-pinterest">&nbsp;</a>&nbsp;</div>
+						</div>
+						<div class="col-md-3 col-sm-6 to_animate text-center" data-animation="fadeInUp">
+							<div class="widget widget_text greylinks topmargin_20">
+								<h3 class="widget-title">Atención al cliente</h3>
+								<p class="margin_0">Bogotá D.C</p>
+								<p class="margin_0">info@company.com</p>
+								<p class="margin_0">+57 (1) 348 7370 Opción 9</p>
+								<p class="margin_0">
+									<a href="mailto:your@mail.com">a-group@support.com</a>
+								</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6 to_animate text-center" data-animation="fadeInUp">
+							<div class="widget widget_text greylinks topmargin_20">
+								<h3 class="widget-title">Atención al empleado</h3>
+								<p class="margin_0">Nacional-Bogotá</p>
+								<p class="margin_0">info@company.com</p>
+								<p class="margin_0">Nacional (+57) 018000127673 </p>
+								<p class="margin_0"> Bogotá +57 (1) 358 1000</p>
+							</div>
+						</div>
+						<div class="col-md-3 col-sm-6 to_animate text-center" data-animation="fadeInUp">
+							<div class="widget widget_nav_menu greylinks topmargin_20">
+								<h3 class="widget-title">Services</h3>
+								<ul class="menu">
+									<li class="">
+										<a href="service-single.html">Web Analytics</a>
+									</li>
+									<li class="">
+										<a href="service-single.html">World Marketing</a>
+									</li>
+									<li class="">
+										<a href="service-single.html">Digital Assests</a>
+									</li>
+									<li class="">
+										<a href="service-single.html">Audit &amp; Assurance</a>
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</footer>
+
+
+			<section class="ls page_copyright table_section section_padding_25">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 text-center">
+							<p class="grey">&copy; Copyright <a href="http://www.serdan.com.co">Serdan 2017</a> Todos los derechos reservados. </p>
+						</div>
+					</div>
+				</div>
+			</section>
